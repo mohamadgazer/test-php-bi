c826b6a025c03e738c0f0c2507c9704cfbf2b309
mohamadgazer <37588351+mohamadgazer@users.noreply.github.com>
2024-03-04 03:26:00 +0200

    Update countries.php

---
+++ countries.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json');
+
+$countries = [
+    'country1' => ['name' => 'Country 1', 'population' => 1000000],
+    'country2' => ['name' => 'Country 2', 'population' => 2000000],
+    'country3' => ['name' => 'Country 3', 'population' => 3000000],
+];
+
+echo json_encode($countries, JSON_PRETTY_PRINT);
